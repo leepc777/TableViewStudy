1f094d1c211d2ee6829bd440a7bfbe45345f5a21
patrick <leepc777@live.com>
2018-04-20 18:18:08 -0700

    Simple TableView. The key is to set the datasource and delegate for the tableView in storyBoard

--- TableViewStudy/ViewController.swift
+++ TableViewStudy/ViewController.swift
@@ -2,24 +2,41 @@
 //  ViewController.swift
 //  TableViewStudy
 //
-//  Created by sam on 4/20/18.
+//  Created by Patrick on 4/20/18.
 //  Copyright © 2018 patrick. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        print("DataSource is \(tableView.dataSource) and Delegate is \(tableView.delegate)")
+        return 2
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tableView.dataSource = self
+        tableView.delegate = self
+        print("the current section \(section)")
+        return 20
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.dataSource = self
+        tableView.delegate = self
 
+        print(" indexPath is \(indexPath) and the row is \(indexPath.row)")
 
+//        return UITableViewCell()
+        let tableViewCell = UITableViewCell()
+        tableViewCell.textLabel?.text = " indexPath is \(indexPath) and row is \(indexPath.row)"
+        return tableViewCell
+    }
 }
 
